f680f689b09feab115bf790d6ae2e1ad12f8beb2
Alex Soldin <alex.soldin@gmail.com>
2024-10-23 10:57:45 +0200

    feat(refactor-py): Write skeleton classes

---
+++ report generation/models/excel_handler.py
@@ -0,0 +1,9 @@
+class ExcelHandler:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def read_excel_file(self):
+        pass
+
+    def write_excel_file(self):
+        pass
