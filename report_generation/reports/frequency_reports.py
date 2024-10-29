6c7f97c5cfe14d6c965ed759d01a97c21160dad8
Natasha Soldin <natashasoldin@Natashas-MacBook-Pro-707.local>
2024-10-29 12:32:32 +0200

    Incorporated color coding styling to frequency reports

--- report_generation/reports/frequency_reports.py
+++ report_generation/reports/frequency_reports.py
@@ -17,24 +17,43 @@ class FrequencyReports:
         Returns:
             A styled DataFrame (Styler) with row colors applied.
         """
-        # Grab the dict from ipynb and use it here for styling
+        color_mapping = {
+            'Loaded for Delivery': '#00aeed',               # Blue
+            'POD Details Captured': '#d0e833',              # Green
+            'POD Image Scanned': '#d0e833',
+            'Attempted delivery': '#f7bc00',                # Orange
+            'Checked in at Origin Depot': '#f7bc00',
+            'Consignment details captured': '#f7bc00',
+            'Event Scan Blocked': '#f7bc00',
+            'Floor check': '#f7bc00',
+            'Inbound Manifest': '#f7bc00',
+            'Manifest Transferred': '#f7bc00',
+            'Mis-routed': '#f7bc00',
+            'Received at origin depot': '#f7bc00',
+            'Remove from manifest/tripsheet': '#f7bc00',
+            'Return to Client': '#f7bc00',
+            'Return to Depot': '#f7bc00',
+            'Reverse logistics floor check': '#f7bc00',
+            'Swadded': '#f7bc00',
+            'Unload manifest/tripsheet': '#f7bc00',
+            'Floor check - Depot collection': '#eac7e6',    # Purple
+            'Chain store floor check': '#fdf900',            # Yellow
+            'Floor check - Booking cargo': '#fdf900',
+            'Outbound Manifest Load': '#fdf900',
+            'Other': 'White'  # Default color for other entries
+        }
 
-        # return df.style.apply(
-        #     lambda row: [
-        #         f"background-color: {LastEventStyles.get_event_style(row['Last Event'])['color']}"
-        #     ]
-        #     * len(row),
-        #     axis=1,
-        # )
-        return df
+        return df.style.apply(
+                lambda row: [f'background-color: {color_mapping.get(row["Last Event"], "white")}'] * len(row),
+                axis=1
+            )
 
     def generate_report(self) -> None:
-        df = self.apply_styles(self.df)
 
-        account_list = df["Account"].unique()
+        account_list = self.df["Account"].unique()
         for account in tqdm(account_list, desc="Generating frequency reports"):
             # Split the DataFrame by Account
-            df_account = df[df["Account"] == account]
+            df_account = self.df[self.df["Account"] == account]
             df_account = df_account.sort_values(
                 by=["Last Event", "Waybill Date"], ascending=[True, False]
             )
@@ -57,14 +76,18 @@ class FrequencyReports:
                 )
             ]
 
+            # Apply styles to each DataFrame before saving
+            styled_current_deliveries_df = self.apply_styles(current_deliveries_df)
+            styled_completed_deliveries_df = self.apply_styles(completed_deliveries_df)
+
             # Save to Excel file with multiple sheets
             with pd.ExcelWriter(
                 f"{self.output_file_path}/{account}.xlsx",
                 engine="openpyxl",
             ) as writer:
-                current_deliveries_df.to_excel(
+                styled_current_deliveries_df.to_excel(
                     writer, sheet_name="Current deliveries", index=False
                 )
-                completed_deliveries_df.to_excel(
+                styled_completed_deliveries_df.to_excel(
                     writer, sheet_name="Completed deliveries", index=False
                 )
