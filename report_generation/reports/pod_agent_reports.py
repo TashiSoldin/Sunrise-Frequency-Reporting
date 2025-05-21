afa13f132513960df2e38911640eb213eb3ab32e
Natasha Soldin <natashasoldin@Natashas-MacBook-Pro-738.local>
2025-05-21 15:22:03 +0200

    Implemented the POD agent report templates

--- report_generation/reports/pod_agent_reports.py
+++ report_generation/reports/pod_agent_reports.py
@@ -1,8 +1,10 @@
+from openpyxl import load_workbook
 import pandas as pd
 from tqdm import tqdm
 
 from helpers.excel_helper import ExcelHelper
 from helpers.datetime_helper import DatetimeHelper
+from helpers.os_helper import OSHelper
 
 
 class PodAgentReports:
@@ -33,11 +35,31 @@ class PodAgentReports:
         ):
             df_agent = df[df["Delivery Agent"] == delivery_agent]
 
+            template_path = OSHelper.load_template(
+                "assets/", "pod_agent_report_template.xlsx"
+            )
+            wb = load_workbook(template_path, data_only=False)
+
+            replacements = {
+                "agent_name": delivery_agent,
+                "date_time": DatetimeHelper.get_precise_current_datetime(),
+                "total_count": len(df_agent),
+            }
+            ExcelHelper.update_template_placeholders(wb, replacements)
+
+            ws = wb.active
+            ExcelHelper.append_df_to_sheet(ws, df_agent, ws.max_row + 2)
+
             file_path = f"{self.output_file_path}/{delivery_agent}-{DatetimeHelper.get_current_datetime()}.xlsx"
-            with pd.ExcelWriter(file_path, engine="xlsxwriter") as writer:
-                df_agent.to_excel(writer, index=False)
+            wb.save(file_path)
 
             ExcelHelper.autofit_excel_file(file_path)
+
+            # file_path = f"{self.output_file_path}/{delivery_agent}-{DatetimeHelper.get_current_datetime()}.xlsx"
+            # with pd.ExcelWriter(file_path, engine="xlsxwriter") as writer:
+            #     df_agent.to_excel(writer, index=False)
+
+            # ExcelHelper.autofit_excel_file(file_path)
 
             summary[delivery_agent] = {
                 "file_path": file_path,
